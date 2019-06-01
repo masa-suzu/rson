234af76a8ada1c864ebbf1bad47535c43e7dac2b
masa-suzu <11026517+masa-suzu@users.noreply.github.com>
2019-06-02 07:02:05 +0900

    Add full test.

--- src/main.rs
+++ src/main.rs
@@ -23,3 +23,61 @@ fn main() {
         Err(e) => println!("{}", e),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::json::Object;
+    use crate::json::Value;
+    use crate::lexer::Lexer;
+    use crate::parser::Error;
+    use crate::parser::Parser;
+    use std::collections::HashMap;
+
+    macro_rules! hash {
+        ( $( $t:expr),* ) => {
+            {
+                let mut temp_hash = HashMap::new();
+                $(
+                    temp_hash.insert($t.0, $t.1);
+                )*
+                temp_hash
+            }
+        };
+    }
+
+    #[test]
+    fn parse_full() {
+        let input = r##"
+        {
+            "x": {
+                "y":3.14
+            },
+            "はろー" : "json"
+        }
+        "##
+        .to_string();
+
+        let want = hash![
+            (
+                "x".to_string(),
+                Value::Object(hash![("y".to_string(), Value::Number(3.14))])
+            ),
+            ("はろー".to_string(), Value::String("json".to_string()))
+        ];
+
+        let mut tokens = Lexer::new(&input).into_iter();
+        let got = Parser::new(&mut tokens).parse();
+
+        assert_object(want, got)
+    }
+
+    fn assert_object(want: Object, got: Result<Object, Error>) {
+        match got {
+            Ok(j) => assert_eq!(want, j),
+            Err(e) => {
+                println!("{:?}", e);
+                assert!(false, "Want Object, got Error {}", e)
+            }
+        }
+    }
+}
